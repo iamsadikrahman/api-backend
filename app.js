b4d116056866e6a439285f1d8ef5fc85ad305628
iamsadikrahman <imasadikrahman@outlook.com>
2023-07-30 23:21:57 +0600

    deploy step

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const data = require('./data.json');
 
-console.log(data)
+console.log(data);
 
 const app = express();
 
-app.get('/api/movies', (req, res) => {
+app.get('/movies', (req, res) => {
   res.json(data);
 });
 
-
 // Define a route to get data by ID
-app.get('/api/movies/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const dataItem = data.find(item => item.id === id);
-    if (dataItem) {
-      res.json(dataItem);
-    } else {
-      res.status(404).json({ message: 'Data not found' });
-    }
-  });
+app.get('/movies/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const dataItem = data.find(item => item.id === id);
+  if (dataItem) {
+    res.json(dataItem);
+  } else {
+    res.status(404).json({ message: 'Data not found' });
+  }
+});
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Movie API!');
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
